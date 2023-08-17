433ffc8ceb04d24a412a49566c3a2438342d7045
unknown <SENA@SGLDFPCATSD262.FormacionSena.RED>
2023-08-17 16:07:35 -0500

    YAA

--- gestion.js
+++ gestion.js
@@ -21,7 +21,7 @@ gestion.use("/api/vendedor", vendedor)
 
 gestion.listen(process.env.PORT, () => {
     console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
+    mongoose.connect(process.env.DB)
+        .then(() => console.log('Connected!'));
 })
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
-    .then(() => console.log('Connected!'));
