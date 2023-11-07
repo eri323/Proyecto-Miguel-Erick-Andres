c9c089f654c2d8bb6b03eec923960ab8a64a5d83
unknown <erickgutierrez085@gmail.com>
2023-11-06 19:29:49 -0500

    ds

--- models/vendedor.js
+++ models/vendedor.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Vendedor = new mongoose.Schema({
     Nombre: { type: String, required: true },
     Cedula:{ type: Number, required: true},
-    password: { type: String, required: true, unique: true },
+    password: { type: String,/*  required: true, */ unique: true },
     Telefono: { type: Number, required: true, unique: true },
     createdAt: { type: Date, default: Date.now },
     estado: { type: Number, default: 1 }
